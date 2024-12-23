5eedd7a120a2cd683d0c7132490dc5674ba1cdbf
Chad Pritchett <chad@chadpritchett.com>
2024-12-23 04:20:10 -0600

    feat: adding mise, rebuilding dev container (#5)

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+sudo apt update -y && sudo apt install -y git
+
+## setup mise
+sudo apt update -y && sudo apt install -y gpg wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
+
+# add a line to the bash profile of the visual studio code user
+echo "eval "$(mise activate bash --shims)"" >> /home/vscode/.bash_profile
